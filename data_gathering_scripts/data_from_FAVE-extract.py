6ff202a677f4ee46221be9d83d15bd09259b7a31
Thomas Leblé <tl78@nyu.edu>
2018-03-29 14:36:17 -0400

    correct_version
    I had pushed a wrong one

--- data_gathering_scripts/data_from_FAVE-extract.py
+++ data_gathering_scripts/data_from_FAVE-extract.py
@@ -1,33 +1,44 @@
 ####
-Gathers the data
+## Gathers the data from formants, using the names in the TextGrids
 ####
+# encoding: utf-8
+import os
+import io
 
-def treat_file(filetotreat):
+TEXTGRIDS_FOLDERS = "/Users/thomasl/Documents/MLProject/FAVE-EXTRACT/"
+FORMANTS_FOLDERS = "/Users/thomasl/Documents/MLProject/FAVE-FORMANTS/"
+RESULT_FOLDERS = "/Users/thomasl/Documents/MLProject/RESULTS/"
+
+
+#######
+### Takes a file with formants (the norm.txt ones)
+### extracts the words with more than 1 syllable
+### and adds (in 'ouput') a data entry of the type
+### word, speaker, [list of the syllables and the formants data for each syllable]
+#####
+def treat_file(filetotreat, output, speaker):
     lines = filetotreat.readlines()
-
-    i=3 # Skip the first three lines
-    current_word = (line[3])[2]
+    current_word = "--"
     word_occurrence = []
 
-    while(i < len(lines)):
-        collapsed_line = collapse_line[lines[i]]
-        if (collapsed_line[0] == current_word):
+    for line in (l.split() for l in lines[3:] if len(l.split()) == 26): ## Skip the first 3 lines, and avoid weird entries
+        collapsed_line = collapse_line(line)
+        if (collapsed_line[0] == current_word or current_word == "--"):
             word_occurrence.append(collapsed_line)
-            current_word = current_word
         else:
-            if len(word_occurrence > 1): ## We only keep words with more than one syllable
+            if (len(word_occurrence) > 1): ## We only keep words with more than one syllable
                 add_occurrence(current_word, word_occurrence, speaker, output)
+            word_occurrence = [collapsed_line]
+        current_word = collapsed_line[0]
 
-            current_word = collapsed_line[0]
-            word_occurence = [collapsed_line]
-            i+=1
     ### Last word of the file
-    if len(word_occurrence > 1):
-    add_occurrence(current_word, word_occurrence, speaker, output)
+    if (len(word_occurrence) > 1):
+        add_occurrence(current_word, word_occurrence, speaker, output)
 
+    return True
 
-def add_occurrence(word, occurence, speaker, output):
-        output.write(word, speaker, str(occurrence))
+def add_occurrence(word, occurrence, speaker, output):
+    output.write(word+','+speaker+','+str(occurrence)+'\n')
 
 def collapse_line(line):
     vowel = line[0]
@@ -51,5 +62,32 @@ def collapse_line(line):
     line_collapsed = [word, vowel, formant_first, formant_second, duration, ff_summary, fs_summary]
     return line_collapsed
 
-output_file = open("result.txt", "a")
-filetotreat = open("2012_11_184_s01_norm.txt")
+
+
+if __name__ == "__main__":
+    os.chdir(TEXTGRIDS_FOLDERS)
+    years = os.listdir() ### List all the years
+    for year in (y for y in years if y.isnumeric()):
+        os.chdir(TEXTGRIDS_FOLDERS+year)
+        list_hearings = os.listdir() ### List all the hearings for a given year
+        for hearing in (h for h in list_hearings if h.endswith('.TextGrid')):
+            hearing_textgrid = open(TEXTGRIDS_FOLDERS+year+'/'+hearing, 'r', encoding='utf-8')
+            hearing_textgrid_lines = hearing_textgrid.readlines()
+            item = 0
+            for line in (l for l in hearing_textgrid_lines if l.strip().startswith("name =")): ### Looking for the speaker name
+                item += 1
+                speaker = (line.strip())[8:-1] ## Only keep the name
+                if (item < 10):
+                    name_file = hearing.strip('.TextGrid')+'_s0'+str(item)+'_norm.txt'
+                else:
+                    name_file= hearing.strip('.TextGrid')+'_s'+str(item)+'_norm.txt'
+                try:
+                    os.chdir(FORMANTS_FOLDERS+year+'_vowels') ### Opening the formant file
+                    filetotreat = open(name_file)
+                except FileNotFoundError:
+                    break
+                os.chdir(RESULT_FOLDERS+year)
+                output = open("result_"+name_file, 'a')
+                treat_file(filetotreat, output, speaker) #### Treating the formant file
+                output.close()
+                filetotreat.close()
